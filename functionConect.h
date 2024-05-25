06896e67e8f32e90c5a36dd92049df37cf22d676
Pedro Pablo <pmontesarroyave@gmail.com>
2024-05-25 18:16:53 -0500

    todo esta melon y las carpeta html esta creada

--- functionConect.h
+++ functionConect.h
@@ -1,30 +1,3 @@
-#include <WiFi.h>
-#include <WiFiClient.h>
-#include <WebServer.h>
-#include <ESPmDNS.h>
-#include <Preferences.h>
-#include "esp_camera.h"
-#include <HTTPClient.h>
-
-//html
-#include "html/index_html.h"              // Incluir el archivo con el HTML
-#include "html/404_html.h"                // Incluir el archivo con el HTML 404
-#include "html/credentials_saved_html.h"  // Incluir el archivo con el HTML de credenciales guardadas
-
-Preferences preferences;
-
-const char* ssid_ap = "ESP32_AP";
-const char* password_ap = "12345678";
-
-WebServer server(80);
-HTTPClient http;
-
-String ssid;
-String password;
-
-unsigned long lastAttemptTime = 0;
-const unsigned long attemptInterval = 300000;  // 5 minutos
-
 void saveCredentials(String ssid, String password) {
   preferences.begin("wifi", false);
   preferences.putString("ssid", ssid);
@@ -100,30 +73,4 @@ void handleSubmit() {
 // Función para manejar el error 404
 void handleNotFound() {
   server.send(404, "text/html", not_found_html);
-}
-
-void setup() {
-  Serial.begin(115200);
-  loadCredentials();
-  if (ssid == "" || password == "") {
-    startAPMode();
-  } else {
-    connectToWiFi();
-  }
-}
-
-void loop() {
-  if (WiFi.status() != WL_CONNECTED) {
-    server.handleClient();  // Manejar las solicitudes del servidor web en modo AP
-    if (millis() - lastAttemptTime > attemptInterval) {
-      lastAttemptTime = millis();
-      Serial.println("Retrying to connect to WiFi...");
-      connectToWiFi();
-    }
-    return;
-  }
-  // Ejecutar funcionalidades adicionales cuando esté conectado al WiFi
-  delay(10000);  // Esperar 10 segundos antes de capturar y enviar otra foto
-  //conecado al wifi
-
 }
